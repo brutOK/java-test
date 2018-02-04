cbbf01a8c1a2b2f9dcf15bcb585070fb20bf3e77
igor <ihorbryt@gmail.com>
2018-02-04 22:37:12 +0200

    test

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,8 @@
 public class Calculate{
     public static void main(String[] args) {
         System.out.println("hello");
+        int a = 1;
+        int b = 1;
+        System.out.println(a + b);
     }
 }
